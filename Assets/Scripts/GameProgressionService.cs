54c69f186f5d823bc947ab2251bf662e005f5237
Aki Aoki <akiko.aoki.0w0@gmail.com>
2022-07-16 23:35:46 +0300

    Added level - see suburban scene 5

---
+++ Assets/Scripts/GameProgressionService.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameProgressionService : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class EnemyVariant
+    {
+        public GameObject enemyPrefab;
+        public float spawnTimeout;
+    }
+    
+    public List<EnemyVariant> enemyVariants;
+    public List<Transform> spawnPoints;
+
+    private float _currentTimeout = 0.0f;
+
+    public void AddEnemyVariant(EnemyVariant enemyVariant) => enemyVariants.Add(enemyVariant);
+
+    public void AddSpawnPoint(Transform spawnPoint) => spawnPoints.Add(spawnPoint);
+
+    private void FixedUpdate()
+    {
+        // Wait for timeout
+        if (_currentTimeout > 0)
+        {
+            _currentTimeout = Mathf.Max(0.0f, _currentTimeout - Time.fixedDeltaTime);
+            return;
+        }
+        
+        // Get a random variant
+        var v = enemyVariants[Random.Range(0, enemyVariants.Count)];
+        // Get a random spawn point
+        var s = spawnPoints[Random.Range(0, spawnPoints.Count)];
+        // Spawn the entity
+        var enemy = Instantiate(v.enemyPrefab, s.transform.position, s.rotation);
+        // Add timeout
+        _currentTimeout += v.spawnTimeout;
+    }
+}
---
+++ Assets/Scripts/TransformCopy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformCopy : MonoBehaviour
+{
+    public Transform target;
+    public bool copyPosition;
+    public bool copyRotation;
+
+    public Vector3 positionOffset;
+    
+    void Update()
+    {
+        if (target == null)
+            return;
+        
+        if (copyPosition)
+            transform.position = target.position + positionOffset;
+
+        if (copyRotation)
+            transform.rotation = target.rotation;
+    }
+}
