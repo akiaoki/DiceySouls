3ca4dbc96afdcc09334813b0b1f47b1ffbbbbfe4
Dmitry <dimaskarakozov@gmail.com>
2022-07-17 12:44:51 +0300

    Heart

---
+++ Assets/DmonPrefabs/Heart.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using MoreMountains.Tools;
+using System.Collections.Generic;
+using System;
+using MoreMountains.Feedbacks;
+namespace MoreMountains.TopDownEngine
+{
+    public class Heart : MonoBehaviour
+    {
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                other.gameObject.MMGetComponentNoAlloc<Health>().GetHealth(1, gameObject);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
